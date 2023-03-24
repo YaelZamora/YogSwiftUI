7bdcb70418907363e19f3b4f37f43338f1275738
YaelZamora <ajedrezkasparov@gmail.com>
2023-03-24 15:55:22 -0600

    Initial Commit

---
+++ yoga/yogaApp.swift
@@ -0,0 +1,17 @@
+//
+//  yogaApp.swift
+//  yoga
+//
+//  Created by Yael Javier Zamora Moreno on 24/03/23.
+//
+
+import SwiftUI
+
+@main
+struct yogaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
